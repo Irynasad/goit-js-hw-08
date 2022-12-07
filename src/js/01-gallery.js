e3ae896adef51b81c8e034fd60c0e080b1abfa69
Iryna Sadychenko <108194331+Irynasad@users.noreply.github.com>
2022-12-07 13:08:09 +0200

    update tesk 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,40 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+
+import SimpleLightbox from 'simplelightbox';
+import SimpleLightbox from 'simplelightbox/dist/simple-lightbox.esm';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
+const containerGallery = document.querySelector('.gallery');
+
+const listOfGallery = createGallery(galleryItems);
+
+containerGallery.insertAdjacentHTML('beforeend', listOfGallery);
+
+function createGallery(items) {
+  return items
+    .map(({ description, original, preview }) => {
+      return `<li>
+                <a class="gallery__item" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                />
+                </a>
+                </li>`;
+    })
+    .join(' ');
+}
+
+const galleryToShow = new SimpleLightbox('.gallery a', {
+  /* options */
+  captionsData: 'alt',
+  captionPosition: 'top',
+  captionDelay: 250,
+  widthRatio: 0.8,
+  heightRatio: 0.8,
+});
 
 console.log(galleryItems);
