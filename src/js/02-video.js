6a8ef315873ebd306c87df728452ce8d5405c89b
Iryna Sadychenko <108194331+Irynasad@users.noreply.github.com>
2022-12-09 02:30:18 +0200

    update task 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const PLAYER_KEY = 'videoplayer-current-time';
 
+// const load = ();
+
+const setWatchingTime = ({ seconds }) => {
+  localStorage.setItem(PLAYER_KEY, JSON.stringify(seconds));
+  console.log(JSON.stringify(seconds));
+};
+const getSaveTime = () => {
+  return JSON.parse(localStorage.getItem(PLAYER_KEY)) || [];
+};
+
+player.on('timeupdate', throttle(setWatchingTime, 1000));
+console.log('played the video!');
+
+// {
+//   duration: 61.857;
+//   percent: 0.049;
+//   seconds: 3.034;
+// }
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+const throttle = require('lodash.throttle');
 
+const LOCAL_KEY = 'feedback-form-state';
+const formData = {};
+const refs = {
+  form: document.querySelector('.feedback-form'),
+};
+
+refs.form.addEventListener('input', onFormInput);
+refs.form.addEventListener('submit', throttle(onFormSubmit, 500));
+
+populateFormInput();
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  event.currentTarget.reset();
+  console.log('Отправляем форму', formData);
+  localStorage.removeItem(LOCAL_KEY);
+}
+function onFormInput(event) {
+  const formData = {
+    email: refs.form.elements.email.value,
+    message: refs.form.elements.message.value,
+  };
+  console.log(formData);
+  console.log(refs.form.elements);
+
+  // formData[event.target.name] = event.target.value;
+  localStorage.setItem(LOCAL_KEY, JSON.stringify(formData.email));
+}
+
+function getSaveInputs() {
+  JSON.parse(localStorage.getItem(LOCAL_KEY));
+}
+
+function populateFormInput(form) {
+  const formData = getSaveInputs(LOCAL_KEY);
+  if (!formData) {
+    return;
+  }
+  for (const key in formData) {
+    form.elements[key].value = formData[key];
+  }
+}
